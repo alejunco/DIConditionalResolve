2ad3165db827641bf7c59ad767df96d38230b1eb
Alejandro Junco <ale911115@gmail.com>
2019-07-17 20:55:07 -0400

    add dependency injection container and register example services

---
+++ DIConditionalResolve/App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DIConditionalResolve
+{
+    class App
+    {
+        private Func<int, ICosa> _serviceAccessor;
+        public App(Func<int, ICosa> serviceAccessor)
+        {
+            _serviceAccessor = serviceAccessor;
+        }
+        public void Run()
+        {
+            var collection = new[] { 1, 2, 3 };
+
+            for (int i = 0; i < collection.Length; i++)
+            {
+                var printerService = _serviceAccessor(collection[i]);
+                printerService.Print();
+            }
+        }
+    }
+}
--- DIConditionalResolve/Program.cs
+++ DIConditionalResolve/Program.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace DIConditionalResolve
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create service collection
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // create service provider
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // entry to run app
+            serviceProvider.GetService<App>().Run();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            // add services
+            serviceCollection.AddSingleton(new CosaPar());
+            serviceCollection.AddSingleton(new CosaImpar());
+
+            // add app
+            serviceCollection.AddTransient<App>();
+
+            serviceCollection.AddTransient<Func<int, ICosa>>(serviceProvider => input =>
+            {
+                
+                switch (input%2==0)
+                {
+                    case true:
+                        {
+                            return serviceProvider.GetService<CosaPar>();
+                        }
+                    case false:
+                        {
+                            var x = serviceProvider.GetService<CosaImpar>();
+                            return x;
+                        }
+                    default:
+                        throw new InvalidOperationException();
+                }
+            });
+
+        }
+    }
+
+    public interface ICosa
+    {
+        void Print();
+    }
+
+    public class CosaPar : ICosa
+    {
+        public void Print()
+        {
+            Console.WriteLine("Es par");
+        }
+    }
+
+    public class CosaImpar : ICosa
+    {
+        public void Print()
+        {
+            Console.WriteLine("Es impar");
         }
     }
 }
