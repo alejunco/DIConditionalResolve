040f54ba69cb5872b39d5e325ee3ded3d2d13315
Alejandro Junco <ale911115@gmail.com>
2019-07-17 20:12:55 -0400

    initial commit

---
+++ DIConditionalResolve/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIConditionalResolve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
